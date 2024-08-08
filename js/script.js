8b1f7ba4b64b211b06aefcb13cc7974fa6db5a63
leonardomatheus9155@gmail.com <leonardomatheus9155@gmail.com>
2024-08-08 20:38:25 -0300

    iniciando com o map

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-// console.log("Bem vindo novamente!");
-
 //Declarando um array de frutas do Brasil.
 let frutas = ["Maçã", "Banana", "Laranja",
     "Manga", "Açaí", "Goiaba", "Ameixa",
     "Cereja", "Mirtilo", "Uva"];
+
+const novaLista = frutas.map(fruta => {
+
+    return frutas.filter(f => f.includes("n"));
+})
+console.log(novaLista);
