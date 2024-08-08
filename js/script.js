e90aa11c8ad84410416f2361d7fdb60d04dd3526
IHateMyTiming <Leo.kawachi@gmail.com>
2024-08-08 20:50:46 -0300

    precos

--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@ let frutas = ["Maçã", "Banana", "Laranja",
 
 const arrayDeObj = frutas.map((fruta, indice) => {
     
-    return {id:indice+1, nome:fruta};
+    return { id: indice + 1, nome: fruta };
 })
-console.log(novaLista)
+console.log(arrayDeObj);
+
+//Array de preço de frutas
+
+let precos = [
+    {id : 1, preco: 5.60},
+    {id : 2, preco: 8.80},
+    {id : 3, preco: 4.50},
+    {id : 4, preco: 7.90},
+    {id : 5, preco: 30.60},
+    {id : 6, preco: 2.30},
+    {id : 7, preco: 10.90},
+    {id : 8, preco: 20.40},
+    {id : 9, preco: 25.55},
+    {id : 10, preco: 9.27},
+];
