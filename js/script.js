229317fd696df8fe2ebd7df1c7902bbeffe797ed
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-08 20:24:21 -0300

    Iniciando projeto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// console.log("Bem vindo novamente!");
+
+//Declarando um array de frutas do Brasil.
+let frutas = ["Maçã", "Banana", "Laranja",
+    "Manga", "Açaí", "Goiaba", "Ameixa",
+    "Cereja", "Mirtilo", "Uva"];
