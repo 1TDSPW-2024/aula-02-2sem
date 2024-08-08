794e8e6688a2f2a9f0212f7a7578adec274801ee
IHateMyTiming <Leo.kawachi@gmail.com>
2024-08-08 20:42:50 -0300

    commit 1

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@
 let frutas = ["Maçã", "Banana", "Laranja",
     "Manga", "Açaí", "Goiaba", "Ameixa",
     "Cereja", "Mirtilo", "Uva"];
+
+const arrayDeObj = frutas.map((fruta, indice) => {
+    
+    return {id:indice+1, nome:fruta};
+})
+console.log(novaLista)
