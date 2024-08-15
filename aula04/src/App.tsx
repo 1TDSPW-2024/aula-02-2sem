e9406a9d1cc8979f21401d7da01b574d7edcc96a
IHateMyTiming <Leo.kawachi@gmail.com>
2024-08-15 20:02:55 -0300

    daf

---
+++ aula04/src/App.tsx
@@ -0,0 +1,10 @@
+//regras da criacao da estrutura do componente funcional
+//1- Deve ser um arquivo com a extensao JSX= JavaScript ou TSX/Typescript
+//2-O nome do arquivo deve iniciar com a letra minuscula. Ex:componente
+//3-A estrutura do cp deve ser igual ao de uma funcao Javascript
+//4-Todo componente deve ter em sua estrutura uma instrucao return() 
+//para poder renderizar o conteudo JSX ou TSX. Obs:os elementos JSX e TSX 
+//deve iniciar neste componente com um elemento de bloco pai. Ex: Div/Header/Main/Select.
+//5- Antes da palavra reservada function devemos adicionar a directiva export default para 
+//informar ao registrador que aquele componente pode ser visto, lido e importado por qualquer 
+//outro componente dentro do projeto.Ex:export deafult function
